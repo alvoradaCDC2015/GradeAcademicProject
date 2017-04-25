ad114e4bf85a3da2b2a660d35574138c1ea73532
slfelipe <fellpsl.18@gmail.com>
2017-04-25 18:52:43 -0300

    RequisitosSemestre
    Manutenção de Ações Semestre.
    Commit para manutenção em Sala de Aula.

--- src/br/com/gradeacademic/servico/AcaoCadastraSemestre.java
+++ src/br/com/gradeacademic/servico/AcaoCadastraSemestre.java
@@ -5,12 +5,34 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 
+import br.com.gradeacademic.entidade.Semestre;
+import br.com.gradeacademic.repositorio.RepositorioSemestre;
 import br.com.gradeacademic.visao.CadastraSemestre;
 
 public class AcaoCadastraSemestre extends CadastraSemestre {
 
 	public static void botaoSalvar(JButton bSalvar) {
+		bSalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 
+				bSalvar.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+
+						Semestre semestre = new Semestre();
+						semestre.setDescricao(tDescricao.getText());
+						semestre.setObservacao(tObservacao.getText());
+
+						RepositorioSemestre.salvar(semestre);
+
+						AcaoCadastraSemestre.internalCadastro.dispose();
+
+					}
+				});
+
+			}
+		});
 	}
 
 	public static void botaoCancelar(JButton bCancelar) {
