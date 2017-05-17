9160430a1dd87ac220b88b0dfb7113e61e1998ce
Guilherme Maeda <gmaeda6@gmail.com>
2017-05-16 21:14:22 -0300

    Update Estado.java
    Criação da entidade Estado

--- src/br/com/gradeacademic/entidade/Estado.java
+++ src/br/com/gradeacademic/entidade/Estado.java
@@ -1,5 +1,53 @@
 package br.com.gradeacademic.entidade;
 
 public class Estado {
+    private int id;
+    private String descricao;
+    private String sigla;
+    private String pais;
+    private int situacao;
+    
+    public int getId() {
+		return id;
+	}
 
+	public void setId(int Id) {
+		this.id = Id;
+	}
+	
+	public String getDescricao(){
+		return descricao;
+	}
+	
+	public void setDescricao(String Descricao){
+		this.descricao = Descricao;
+		
+	}
+    
+	
+	public String getSigla(){
+		return sigla;
+	}
+	
+	public void setSigla(String Sigla){
+		this.sigla = Sigla;
+		
+	}
+	
+	public String getPais(){
+		return pais;
+	}
+	
+	public void setPais(String Pais){
+		this.pais = Pais;
+		
+	}
+	
+	public int getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(int Situacao) {
+		this.situacao = Situacao;
+	}
 }
