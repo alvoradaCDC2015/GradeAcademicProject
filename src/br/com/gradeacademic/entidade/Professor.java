00d9394d4b871466b7f8071f710c34e09b2075f0
Guilherme Maeda <gmaeda6@gmail.com>
2017-05-16 21:13:27 -0300

    Update Professor.java
    Criação da entidade professor

--- src/br/com/gradeacademic/entidade/Professor.java
+++ src/br/com/gradeacademic/entidade/Professor.java
@@ -1,5 +1,132 @@
 package br.com.gradeacademic.entidade;
 
 public class Professor {
+	private int id;
+	private String nome;
+	private int dataNascimento;
+	private int cpf;
+	private int situacao;
+	private String nacionalidade;
+	private String naturalidade;
+	private int coordenador;
+	private String endereco;
+	private int numero;
+	private String bairro;
+	private int cep;
+	private String estado;
+	private String cidade;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int Id) {
+		this.id = Id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String Nome) {
+		this.nome = Nome;
+	}
+
+	public int getDataNascimaento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(int DataNascimento) {
+		this.dataNascimento = DataNascimento;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int Cpf) {
+		this.cpf = Cpf;
+	}
+
+	public int getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(int Situacao) {
+		this.situacao = Situacao;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String Nacionalidade) {
+		this.nacionalidade = Nacionalidade;
+	}
+
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+
+	public void setNaturalidade(String Naturalidade) {
+		this.naturalidade = Naturalidade;
+	}
+
+	public int getCoordenador() {
+		return coordenador;
+	}
+
+	public void setCoordenador(int Coordenador) {
+		this.coordenador = Coordenador;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String Endereco) {
+		this.endereco = Endereco;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int Numero) {
+		this.numero = Numero;
+	}
+	
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String Bairro) {
+		this.bairro = Bairro;
+	}
+	
+	public int getCep() {
+		return cep;
+	}
+
+	public void setCep(int Cep) {
+		this.cep = Cep;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String Estado) {
+		this.estado = Estado;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String Cidade) {
+		this.cidade = Cidade;
+	}
+
 
 }
